ba85030fa0f11b9fb71ca5dfa5609fa7cff1f293
Tuan Phan <40224679+tuanbbhero@users.noreply.github.com>
2025-05-26 08:12:45 +0700

    Update scf.js

--- scf.js
+++ scf.js
@@ -1,61 +1,6 @@
-var tpProjectCustomFields = function() {
-  if ($('.summary-item').length > 0) {
-    var tpSummaryItems = $('.summary-item');
-    var totalItems = tpSummaryItems.length;
-    var batchSize = 5;
-    var delayBetweenBatches = 500;
-    
-    function processBatch(startIndex) {
-      var endIndex = Math.min(startIndex + batchSize, totalItems);
-      
-      for (var i = startIndex; i < endIndex; i++) {
-        (function(index) {
-          var tpSummaryItem = $(tpSummaryItems[index]);
-          var tpSummaryLink = tpSummaryItem.find('a.summary-title-link');
-          var tpProjectUrl = tpSummaryLink.attr('href');
-          
-          if (tpProjectUrl && tpSummaryLink.length && tpSummaryLink.prev('.summary-custom-field').length === 0) {
-            var cacheBuster = '?nocache=' + new Date().getTime() + Math.random();
-            $.get(tpProjectUrl + cacheBuster, function(tpData) {
-              var tpCustomField = $(tpData).find('p.custom-field').clone();
-              
-              if (tpCustomField.length) {
-                if (tpSummaryLink.prev('.summary-custom-field').length === 0) {
-                  tpCustomField.addClass('summary-custom-field');
-                  tpSummaryLink.before(tpCustomField);
-                  
-                  tpCustomField.css({
-                    'opacity': '0.8'
-                  });
-                }
-              }
-            });
-          }
-        })(i);
-      }
-      
-      if (endIndex < totalItems) {
-        setTimeout(function() {
-          processBatch(endIndex);
-        }, delayBetweenBatches);
-      }
-    }
-    
-    processBatch(0);
-  }
-};
-var tpCustomFieldsInitialized = false;
-function initializeCustomFields() {
-  if (!tpCustomFieldsInitialized) {
-    tpCustomFieldsInitialized = true;
-    setTimeout(function() {
-      tpProjectCustomFields();
-    }, 1000);
-  }
-}
-$(document).ready(function() {
-  initializeCustomFields();
-});
-$(window).on('load', function() {
-  initializeCustomFields();
-});
+/*
+ * By: tuanphan
+ * Date: 26-05-2025
+ * Note: I offer free install, free customize, you can email me.
+ */
+var tpProjectCustomFields=function(){if($('.summary-item').length>0){var a=$('.summary-item'),b=a.length,c=5,d=500;function e(f){var g=Math.min(f+c,b);for(var h=f;h<g;h++){(function(i){var j=$(a[i]),k=j.find('a.summary-title-link'),l=k.attr('href');if(l&&k.length&&k.prev('.summary-custom-field').length===0){var m='?nocache='+new Date().getTime()+Math.random();$.get(l+m,function(n){var o=$(n).find('p.custom-field').clone();if(o.length){if(k.prev('.summary-custom-field').length===0){o.addClass('summary-custom-field');k.before(o);o.css({opacity:'0.8'})}}})}})(h)}if(g<b){setTimeout(function(){e(g)},d)}}e(0)}},tpCustomFieldsInitialized=false;function initializeCustomFields(){if(!tpCustomFieldsInitialized){tpCustomFieldsInitialized=true;setTimeout(function(){tpProjectCustomFields()},1000)}}$(document).ready(function(){initializeCustomFields()});$(window).on('load',function(){initializeCustomFields()});
